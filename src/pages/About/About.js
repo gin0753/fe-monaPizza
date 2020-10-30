8b09d925cb51c657011015300fce267ba457bf45
Gin Huang <ginrobin0204@gmail.com>
2020-10-30 21:47:02 +1000

    #Feature - add about header & footer

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,15 +7,42 @@ import FrontPage from "./components/FrontPage";
 import Gallery from "./components/Gallery/Gallery";
 import Display from "./components/Display/Display";
 import Questions from "./components/Questions/Questions";
+import Header from '../../components/Header/Header';
+import Newsletter from '../../components/Newsletter/Newsletter';
+import Media from '../../components/Media/Media';
+import Footer from '../../components/Footer/Footer';
+import SideBar from '../../components/SideBar/SideBar/SideBar';
+import CartTotals from '../../components/Cart/CartTotal/CartTotal';
+
 
 class About extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
+    this.state = {
+      sidebarIsClicked: false,
+      cartIsClicked: false
+    }
+  }
+
+  toggleSideBar = () => {
+    this.setState({
+      sidebarIsClicked: !this.state.sidebarIsClicked
+    })
+  }
+
+  toggleCart = () => {
+    this.setState({
+      cartIsClicked: !this.state.cartIsClicked
+    })
   }
 
   render() {
     return (
       <section className='about'>
+        <SideBar sidestatus={this.state.sidebarIsClicked}/>
+        <CartTotals cartstatus={this.state.cartIsClicked}/>
+        <Header toggleSideBar={this.toggleSideBar} toggleCart={this.toggleCart} 
+        sidestatus={this.state.sidebarIsClicked} cartstatus={this.state.cartIsClicked}/>
         <FrontPage />
         <div className='restPage'>
           <Info />
@@ -25,6 +52,9 @@ class About extends React.Component {
           <Questions />
           <Gallery />
         </div>
+        <Newsletter />
+        <Media />
+        <Footer />
       </section>
     );
   }

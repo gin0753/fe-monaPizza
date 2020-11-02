b2925470d43ca6d8afed5fbb21149ff6f2e3997f
bwlaq <carnegielu@gmail.com>
2020-11-02 17:32:32 +1000

    rename customer-details to checkout

---
+++ src/pages/Checkout/components/Newsletter/components/Email/Email.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../Newsletter.css'
+
+class Email extends React.Component{
+
+    render(){
+        return  <div className="container__emailwrapper">
+                    <input type="text" name="email" placeholder="Enter your email address"/>
+                    <label>
+                        <button type="button" className="subscribeButton">SUBSCRIBE</button>
+                    </label>
+                </div>     
+    }
+}
+
+export default Email;

f35ae36a88a85f8b7365f622986eaf4abf5d2e0e
Gin Huang <ginrobin0204@gmail.com>
2020-11-07 15:18:44 +1000

    #Feature - login authentication

--- src/components/SignIn/Login/Login.js
+++ src/components/SignIn/Login/Login.js
@@ -3,11 +3,118 @@ import '../SignIn.css';
 import facebook from '../../../images/facebook_icon.svg';
 import wechat from '../../../images/wechat_icon.svg';
 import google from '../../../images/google_icon.svg';
+import axios from 'axios';
+import GoogleLogin from 'react-google-login';
 
 class Login extends React.Component{
     constructor(props){
         super(props)
-        this.state = {isClicked: false};
+        this.state = {
+            isClicked: false,
+            isLogin: false,
+            UserName: '',
+            SurName: '',
+            Email: '',
+            Password: ''
+        };
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    clickRegister = async (e) => {
+        e.preventDefault();
+
+        const RegisterInfo = {
+            UserName: this.state.Username,
+            SurName: this.state.Surname,
+            Email: this.state.Email,
+            Password: this.state.Password
+        }
+
+        try{
+            const response = await axios.post('/register', RegisterInfo);
+            if(response.status === 201){
+                console.log(response.data);
+            }
+            else{
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    clickSignIn = (e) => {
+        e.preventDefault();
+
+        const LoginInfo = {
+            Email: this.state.Email,
+            Password: this.state.Password,
+        }
+
+        // const config = {
+        //     headers: {
+        //         Authorization: `Bearer ${sessionStorage.getItem('login-token')}`
+        //     }
+        // }
+
+        const loginRequest = async () => {
+            try{
+                // console.log(config)
+                const response = await axios.post(
+                    '/login', 
+                    LoginInfo,
+                    // config
+                    );
+                if(response.status === 200){
+                    this.setState({
+                        isLogin: true
+                    });
+                    console.log(response);
+                    sessionStorage.setItem('login-token', response.data.token);
+                }
+                else{
+                    sessionStorage.setItem('login-token', null);
+                }
+            } catch (err) {
+                console.log(err);
+                sessionStorage.setItem('login-token', null);
+            }
+        }
+        
+        loginRequest();  
+    }
+    responseGoogle = async (res) => {
+
+        let { googleId, profileObj, tokenObj } = res;
+        let { email, name } = profileObj;
+        let { access_token } = tokenObj;
+        let { givenName } = name;
+
+        const googleInfo = {
+            GoogleID: googleId,
+            UserName: name,
+            SurName: givenName,
+            Email: email,
+            Password: access_token
+        };
+        try{
+            const response = await axios.post('/googleLogin', googleInfo);
+            if(response.status === 200 || response.status === 201){
+                console.log(response);
+                sessionStorage.setItem('login-token', response.data.token);
+            }
+            else{
+                sessionStorage.setItem('login-token', null);
+            }
+        } catch (err){
+            console.log(err);
+            sessionStorage.setItem('login-token', null);
+        }
     }
 
     toggleLogin = (props) => {
@@ -19,15 +126,24 @@ class Login extends React.Component{
                         <h5>Login using social networks</h5>
                         <div className="Login__login--logincontainer--social">
                             <i><a><img src={facebook} alt="facebook_icon"/></a></i>
-                            <i><a><img src={google} alt="google_icon"/></a></i>
+                            <GoogleLogin
+                                clientId="124030455752-ctj88mk1vjbueamnf47fcjsp5p4ugpqn.apps.googleusercontent.com"
+                                buttonText="Login"
+                                onSuccess={this.responseGoogle}
+                                onFailure={this.responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                                render={renderProps =>(
+                                    <i><a onClick={renderProps.onClick}><img src={google} alt="google_icon"/></a></i>
+                                )}
+                            />
                             <i><a><img src={wechat} alt="wechat_icon"/></a></i>
                         </div>
                         <div className="Login__login--logincontainer--titlewrapper">
                             <span>OR</span>
                         </div>
-                        <input type="text" id="username" name="username" placeholder="Email"/>
-                        <input type="password" id="password" name="password" placeholder="Password"/>
-                        <button>Sign In</button>
+                        <input type="text" name="Email" placeholder="Email" onChange={this.handleChange}/>
+                        <input type="password" name="Password" placeholder="Password" onChange={this.handleChange}/>
+                        <button onClick={this.clickSignIn}>Sign In</button>
                     </div>
                 </div>
             )
@@ -46,15 +162,15 @@ class Login extends React.Component{
                         <div className="Login__login--registercontainer--titlewrapper">
                             <span>OR</span>
                         </div>
-                        <input type="text" id="username" name="username" placeholder="Username"/>
-                        <input type="text" id="surname" name="surname" placeholder="Surname"/>
-                        <input type="text" id="email" name="email" placeholder="Email"/>
-                        <input type="password" id="password" name="password" placeholder="Password"/>
+                        <input type="text" id="username" name="Username" placeholder="Username" onChange={this.handleChange}/>
+                        <input type="text" id="surname" name="Surname" placeholder="Surname" onChange={this.handleChange}/>
+                        <input type="text" id="email" name="Email" placeholder="Email" onChange={this.handleChange}/>
+                        <input type="password" id="password" name="Password" placeholder="Password" onChange={this.handleChange}/>
                         <div>
                             <input type="checkbox" id="term" name="term" value="term"/>
                             <label>I have read the <a>Term & Conditions</a></label>
                         </div>
-                        <button>Sign In</button>
+                        <button onClick={this.clickRegister}>Sign Up</button>
                     </div>
                 </div>
             )

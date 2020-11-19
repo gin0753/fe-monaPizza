145ebde2eef1bfbb189be1adab11b33f324dbd7c
JYM Jiang <jym@JYM-MBP.local>
2020-11-19 22:39:21 +1030

    fetch state from store

--- src/reducer/shoppingCartReducer.js
+++ src/reducer/shoppingCartReducer.js
@@ -1,37 +1,34 @@
 const defaultState = {
+  productList: [
+    {
+      name: "Moorish Lamb",
+      size: "Large",
+      price: 19.0,
+      qty: 1,
+      totalPrice: 19.0,
+    },
+  ],
 
-    productList: [
-        {
-            name: "Moorish Lamb",
-            size: "Large",
-            price: 19.00,
-            qty: 1,
-            totalPrice: 19.00
-        }
-    ],
-    
-    discount: 0,
+  discount: 0,
 
-    cartSubtotal: 79.00,
+  cartSubtotal: 79.0,
 
-    orderTotal: 79.00
-}
+  orderTotal: 79.0,
+};
 
 export const shoppingCartReducer = (state = defaultState, action) => {
+  const newState = JSON.parse(JSON.stringify(state));
 
-    const newState = JSON.parse(JSON.stringify(state));
+  switch (action.type) {
+    case "ADD_DISCOUNT":
+      newState.discount = action.discount;
+      return newState;
 
-    switch(action.type) {
-        
-        case "ADD_DISCOUNT":
-            newState.discount = action.discount;
-            return newState;
-        
-        case "UPDATE_TOTAL_PRICE":
-            newState.orderTotal = action.cartSubtotal - action.discount;
-            return newState;
-        
-        default:
-            return state;
-    }
-}
+    case "UPDATE_TOTAL_PRICE":
+      newState.orderTotal = action.cartSubtotal - action.discount;
+      return newState;
+
+    default:
+      return state;
+  }
+};

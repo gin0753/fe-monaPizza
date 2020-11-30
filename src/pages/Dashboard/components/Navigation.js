bad610146a2b9d05678e4262faa1865f7d99cb94
NeoC <neochen04@gmail.com>
2020-12-01 00:06:51 +1100

    mydetails page responsive website design

--- src/pages/Dashboard/components/Navigation.js
+++ src/pages/Dashboard/components/Navigation.js
@@ -3,17 +3,17 @@ import './Navigation.css';
 import Title from './components/Title/Title';
 import NavBar from './components/NavBar/NavBar';
 
-class Navigation extends React.Component{
+class Navigation extends React.Component {
 
-    render(){
-        return <div className="Checkout">
-                    <div className="navigation">
-                        <div className="container">
-                            <Title />
-                            <NavBar />
-                        </div>
-                    </div> 
-               </div>    
+    render() {
+        return <div className="breadcrumb">
+            <div className="navigation">
+                <div className="container">
+                    <Title />
+                    <NavBar />
+                </div>
+            </div>
+        </div>
     }
 }
 

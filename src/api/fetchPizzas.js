af5ac6c5633e944eaf27e53bb42fdbd56f6ed9ef
Jeng-han Lu <jeng.han.lu@gmail.com>
2020-12-19 21:29:58 +1000

    fix all APIs

--- src/api/fetchPizzas.js
+++ src/api/fetchPizzas.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import API from '../config/app'
 
 export const fetchPizzas = async () => {
-  const res = await axios.post('menu/1/8');
+  const res = await axios.post(API.backend_api + 'menu/1/8');
   return res;
 };
